134b60f08af265b98ab5e498a2cde9d58cc1fd74
Domenico Pagano <dpaganoh@gmail.com>
2025-10-03 17:08:53 -0500

    github icon on footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,15 @@
+import { FaGithub } from "react-icons/fa";
+
 export default function Footer(){
     return(
-        <footer className="bg-gray-800 p-2 flex justify-center">
+        <footer className="bg-gray-800 p-2 flex justify-center items-center gap-2">
             <p className="text-white text-xs">Pokedex creado por &copy; Gnomono</p>
+            <a 
+                href="https://github.com/DomenicoPH"
+                target="_blank"
+                rel="nonopener noreferrer"
+                className="text-white hover:text-gray-400 text-sm"
+            ><FaGithub/></a>
         </footer>
     )
 }
