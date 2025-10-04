bb44a91b5df4af4748c4098734d9ffcb5649749a
Domenico Pagano <dpaganoh@gmail.com>
2025-10-04 12:55:59 -0500

    ajustes en Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,14 @@
 export default function Navbar(){
+
+    const pokeicon = '/src/assets/img/pokeicon.png'
     return(
         <nav className="flex justify-center items-center h-10 bg-yellow-400 fixed w-screen z-10">
             <div className="flex justify-between w-[80vw]">
-                <h2 className="font-poke"><a href="/">Pokédex</a></h2>
-                <div className="flex gap-4">
+                <div className="flex items-center gap-1">
+                    <img src={pokeicon} alt="Pokedex logo" className="h-[25px]" />
+                    <h2 className="font-poke text-xs pt-1"><a href="/">Pokédex</a></h2>
+                </div>
+                <div className="flex items-center gap-4 text-xs">
                     <a href="/types">Types</a>
                     <a href="/about">About</a>
                 </div>
