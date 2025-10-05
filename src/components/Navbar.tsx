645513c5091a50e2ac31c7b2443f72bc97cf7729
Domenico Pagano <dpaganoh@gmail.com>
2025-10-05 18:19:01 -0500

    ajuste en Navbar (anchor to Link) para correcto redireccionamiento en deploy

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import pokeicon from '../assets/img/pokeicon.png'
 
 export default function Navbar(){
@@ -6,7 +7,7 @@ export default function Navbar(){
             <div className="flex justify-between w-[80vw]">
                 <div className="flex items-center gap-1">
                     <img src={pokeicon} alt="Pokedex logo" className="h-[25px]" />
-                    <h2 className="font-poke text-xs pt-1"><a href="/">Pokédex</a></h2>
+                    <h2 className="font-poke text-xs pt-1"><Link to="/">Pokédex</Link></h2>
                 </div>
                 <div className="flex items-center gap-4 text-xs">
                     <a href="/types">Types</a>
