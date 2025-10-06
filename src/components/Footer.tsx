323c4dfa75e9c67230f4d079a3d0d1c367655c18
Domenico Pagano <dpaganoh@gmail.com>
2025-10-05 19:48:55 -0500

    landing completo + ajustes en footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,15 @@
 import { FaGithub } from "react-icons/fa";
+import vite from '../../public/vite.svg';
 
 export default function Footer(){
     return(
         <footer className="bg-gray-800 p-2 flex justify-center items-center gap-2">
-            <p className="text-white text-xs">Pokedex creado por &copy; Gnomono</p>
+            <p className="flex items-center gap-2 text-white text-[10px]">App built with React Vite <img src={vite} alt="logo Vite" width={16} /> by &copy; Gnomono</p>
             <a 
                 href="https://github.com/DomenicoPH"
                 target="_blank"
                 rel="nonopener noreferrer"
-                className="text-white hover:text-gray-400 text-sm"
+                className="text-white hover:text-gray-400 text-[16px]"
             ><FaGithub/></a>
         </footer>
     )
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,16 +1,22 @@
 import { Link } from 'react-router-dom';
 import logo from '../assets/img/pokeicon.png';
+import vite from '../../public/vite.svg';
 
 export default function Landing(){
     return(
         <div className="flex flex-col justify-center items-center min-h-[100vh] bg-red-500">
-            <Link to="/home">
+            {/* <h2 className='text-lg opacity-50'>- Pokedex -</h2> */}
+            <Link to="/home" className='group flex flex-col items-center'>
                 <img 
                     src={logo} 
                     alt="Logo pokemon" 
-                    className='hover:scale-110 transition-all cursor-pointer'
+                    className='scale-90 group-hover:scale-100 transition-all duration-700 cursor-pointer opacity-20 rotate-90 hover:rotate-0'
                 />
+                <span className="opacity-0 group-hover:opacity-100 text-black/20 text-sm mt-2 transition-opacity">
+                  Enter
+                </span>
             </Link>
+            <footer className='flex items-center gap-2 fixed bottom-10 text-xs text-red-700'>App built with React Vite <img src={vite} alt="logo Vite" width={16} /> by Gnomono</footer>
         </div>
     )
 }
