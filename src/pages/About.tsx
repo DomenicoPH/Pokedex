ae05f7bf3d9f446fe11464008a96480945545764
Domenico Pagano <dpaganoh@gmail.com>
2025-10-12 00:06:12 -0500

    fix about

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -13,33 +13,33 @@ export default function About() {
         Hi there! I'm <a href="https://portfolio-gnomono.vercel.app/" target="_blank" rel="noopener noreferrer" className="font-semibold px-2 text-yellow-400">Gnomono</a>, a web developer passionate about frontend development and modern technologies. This Pokédex was built as a personal project to practice React and explore the <a href="https://pokeapi.co/" target="_blank" rel="noopener noreferrer" className="font-semibold px-2 text-yellow-400"> PokeAPI</a>— a public API that provides detailed data about every Pokémon, including their types, abilities, and much more.
       </p>
 
-      <div className="max-w-2xl bg-yellow-400 text-gray-600 rounded-2xl p-6 backdrop-blur-sm shadow-lg mb-8">
-        <h2 className="text-md font-normal mb-6">Technologies Used</h2>
-        <div className="flex flex-wrap justify-center gap-10 px-10 text-gray-700">
-          <div className="flex flex-col items-center text-[8px] max-w-20">
-            <SiVite size={36} className="mb-1" />
+      <div className="max-w-2xl text-white rounded-2xl p-6 backdrop-blur-sm mb-8">
+        <h2 className="text-md font-normal mb-8">Technologies Used</h2>
+        <div className="flex flex-wrap justify-center gap-10 px-10 text-yellow-400">
+          <div className="flex flex-col gap-2 items-center text-[8px] max-w-30">
+            <SiVite size={50} className="mb-1" />
             Vite
           </div>
-          <div className="flex flex-col items-center text-[8px] max-w-20">
-            <SiReact size={36} className="mb-1" />
+          <div className="flex flex-col gap-2 items-center text-[8px] max-w-30">
+            <SiReact size={50} className="mb-1" />
             React 19
           </div>
-          <div className="flex flex-col items-center text-[8px] max-w-20">
-            <SiTailwindcss size={36} className="mb-1" />
+          <div className="flex flex-col gap-2 items-center text-[8px] max-w-30">
+            <SiTailwindcss size={50} className="mb-1" />
             Tailwind CSS
           </div>
-          <div className="flex flex-col items-center text-[8px] max-w-20">
-            <SiAxios size={36} className="mb-1" />
+          <div className="flex flex-col gap-2 items-center text-[8px] max-w-30">
+            <SiAxios size={50} className="mb-1" />
             Axios
           </div>
-          <div className="flex flex-col items-center text-[8px] max-w-20">
-            <SiReactrouter size={36} className="mb-1" />
+          <div className="flex flex-col gap-2 items-center text-[8px] max-w-30">
+            <SiReactrouter size={50} className="mb-1" />
             React Router
           </div>
         </div>
       </div>
 
-      <p className="text-xs opacity-80 leading-loose">
+      <p className="text-[6pt] opacity-80 leading-loose">
         All Pokémon data and images are © Nintendo / Game Freak / The Pokémon Company.
         <br />
         This project was made for educational and practice purposes only.
