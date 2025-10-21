a745b5c4e5a480e7ac0bf7f6c8ab931009bb4bff
Domenico Pagano <dpaganoh@gmail.com>
2025-10-20 19:45:04 -0500

    gsap install + gsap animations on Landing.tsx

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,13 +1,18 @@
 import { Link } from 'react-router-dom';
 import logo from '../assets/img/pokeicon.png';
 import vite from '../assets/vite.svg';
+import { gsap } from 'gsap';
 
 export default function Landing(){
+
+    gsap.fromTo('#pokemon-enter', {opacity: 0, scale: 2},{opacity: 1, scale: 1, duration: 1});
+    gsap.fromTo('#pokemon-footer', {opacity: 0, y: 20},{opacity: 1, y:0, duration: 1.5});
+
     return(
         <div className="flex flex-col justify-center items-center min-h-[100vh] bg-red-500">
             {/* <h2 className='text-lg opacity-50'>- Pokedex -</h2> */}
-            <Link to="/home" className='group flex flex-col items-center'>
-                <img 
+            <Link id='pokemon-enter'  to="/home" className='group flex flex-col items-center'>
+                <img
                     src={logo} 
                     alt="Logo pokemon" 
                     className='scale-90 group-hover:scale-100 transition-all duration-700 cursor-pointer opacity-20 rotate-90 hover:rotate-0'
@@ -16,7 +21,7 @@ export default function Landing(){
                   Enter
                 </span>
             </Link>
-            <footer className='flex items-center gap-2 fixed bottom-10 text-xs text-red-700'>App built with React Vite <img src={vite} alt="logo Vite" width={16} /> by Gnomono</footer>
+            <footer id='pokemon-footer' className='flex items-center gap-2 fixed bottom-10 text-xs text-red-700'>App built with React Vite <img src={vite} alt="logo Vite" width={16} /> by Gnomono</footer>
         </div>
     )
 }
